14160abd213529d522f6feffa36bd929d5d0dd9c
Phil Chaplin <pchapl1@gmail.com>
2025-10-21 12:30:33 -0700

    updated tests to meet 85% threshhold and updated coveragerc to omit testing manage.py

--- server/recipes/tests/test_recipes_api.py
+++ server/recipes/tests/test_recipes_api.py
@@ -22,3 +22,32 @@ class RecipeAPITests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Recipe.objects.count(), 1)
         self.assertEqual(Recipe.objects.get().title, 'Pancakes')
+
+    def test_list_recipes(self):
+        Recipe.objects.create(user=self.user, title='Toast', instructions='Toast bread')
+        url = reverse('recipe-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(response.data), 1)
+
+    def test_retrieve_recipe(self):
+        recipe = Recipe.objects.create(user=self.user, title='Soup', instructions='Boil water')
+        url = reverse('recipe-detail', args=[recipe.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Soup')
+
+    def test_update_recipe(self):
+        recipe = Recipe.objects.create(user=self.user, title='Pasta', instructions='Boil')
+        url = reverse('recipe-detail', args=[recipe.id])
+        response = self.client.patch(url, {'title': 'Pasta Updated'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, 'Pasta Updated')
+
+    def test_delete_recipe(self):
+        recipe = Recipe.objects.create(user=self.user, title='Salad', instructions='Mix')
+        url = reverse('recipe-detail', args=[recipe.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Recipe.objects.count(), 0)
